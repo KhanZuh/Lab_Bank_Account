f7f121d00652a2f01b5ebd915739d8138862f6eb
KhanZuh <khanzuhair96@live.co.uk>
2024-05-10 08:51:37 +0100

    Adding Bank Account Lab

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,58 @@
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount (String firstName, String lastName, String dateOfBirth, String accountNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.accountNumber = accountNumber;
+        this.balance = 0.0; // Balance starts at 0
+    }
+
+//  Access modifiers - Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+//  Access modifiers - Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+}
